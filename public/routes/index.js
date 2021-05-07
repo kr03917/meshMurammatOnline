c8c86e16c0014a55461aecf6b3b839ead4c6ae51
kr03917 <31242820+kr03917@users.noreply.github.com>
2021-05-07 06:25:30 +0500

    Update index.js

--- public/routes/index.js
+++ public/routes/index.js
@@ -68,7 +68,7 @@ router.post('/uploadfile', upload.single('mesh'), (req, res, next) => {
 
   var fun =function(){
    console.log("fun() start");
-   exec(("wine '", path.join(__dirname, '/meshtype.exe ', file.path, "'").toString())
+   exec("wine ", path.join("'", __dirname, '/meshtype.exe ', file.path, "'")
    //exec(path.join(__dirname, '/conversion_exe.exe'), [file.originalname, 'ply'], { cwd: __dirname }).toString()
   };  
 
