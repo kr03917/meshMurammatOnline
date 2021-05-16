f65b18efcd76872a7d594166529d50730f679dc7
kr03917 <31242820+kr03917@users.noreply.github.com>
2021-05-17 04:37:48 +0500

    ss

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', indexRouter);
-app.listen(80);
+app.listen(8000);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
